ad0885aa1ce4c995d977937e327cb2199dbcd6a2
EmilyDSarani <emily.sarani@gmail.com>
2021-07-15 18:46:41 -0500

    tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,52 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+
+import { add } from '../utils.js';
+import { subtract } from '../utils.js';
+import { multiply } from '../utils.js';
+import { divide } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+test('add should take in 10 and 3 and return 13', (expect) => {
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+    const expected = 13;
+    
+    
+    const actual = add(10, 3);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+  
+    expect.equal(actual, expected);
+});
+
+test('subtract should take in 10 and 5 and return 5', (expect) => {
+    
+    const expected = 5;
+    
+    
+    const actual = subtract(10, 5);
+
+   
+    expect.equal(actual, expected);
+});
+
+
+test('multiply should take in 5 and 5 return 25', (expect) => {
+    
+    const expected = 25;
+    
+    
+    const actual = multiply(5, 5);
+
+    
+    expect.equal(actual, expected);
+});
+
+test('divide should take in "10" and 2 return 5', (expect) => {
+  
+    const expected = 5;
+    
+    
+    const actual = divide('10', 2);
+
+    
     expect.equal(actual, expected);
 });
